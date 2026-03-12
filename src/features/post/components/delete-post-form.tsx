ef222c2f9f7c3cee4b027a875d1c7d308d75ba74
Levy Prado <levyprado28@gmail.com>
2026-03-12 16:41:04 -0400

    feat: create delete post form component

---
+++ src/features/post/components/delete-post-form.tsx
@@ -0,0 +1,51 @@
+import Button from '@/components/ui/button'
+import Icon from '@/components/ui/icon'
+import { Loading03Icon } from '@hugeicons/core-free-icons'
+import { useDeletePost } from '../mutations'
+import { usePostActionDialog } from './post-action-dialog'
+
+type DeletePostFormProps = {
+  postId: number
+}
+
+export default function DeletePostForm({ postId }: DeletePostFormProps) {
+  const { close } = usePostActionDialog()
+  const deletePostMutation = useDeletePost(postId)
+
+  const handleDelete = async () => {
+    try {
+      await deletePostMutation.mutateAsync()
+      close()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <div className='flex justify-end gap-3'>
+      <Button
+        onClick={close}
+        disabled={deletePostMutation.isPending}
+        variant='outline'
+        className='px-8'
+      >
+        Cancel
+      </Button>
+      <Button
+        onClick={handleDelete}
+        disabled={deletePostMutation.isPending}
+        variant='destructive'
+        className='px-8'
+      >
+        {deletePostMutation.isPending ? (
+          <>
+            <Icon icon={Loading03Icon} size={20} className='animate-spin' />
+            Deleting...
+          </>
+        ) : (
+          'Delete'
+        )}
+      </Button>
+    </div>
+  )
+}
