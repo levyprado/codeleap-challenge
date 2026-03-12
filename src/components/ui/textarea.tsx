f121f547aca2e2fb644895afe29192742f060ff2
Levy Prado <levyprado28@gmail.com>
2026-03-11 20:36:05 -0400

    feat: add aria-invalid styles to textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react'
-
 import { cn } from '@/lib/utils'
 
 export default function Textarea({
@@ -10,7 +8,7 @@ export default function Textarea({
     <textarea
       data-slot='textarea'
       className={cn(
-        'flex field-sizing-content max-h-32 min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 shadow-sm transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-accent focus-visible:ring-2 focus-visible:ring-accent/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:bg-input/30 dark:disabled:bg-input/80',
+        'flex field-sizing-content max-h-32 min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 shadow-sm transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-accent focus-visible:ring-2 focus-visible:ring-accent/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
         className,
       )}
       {...props}
